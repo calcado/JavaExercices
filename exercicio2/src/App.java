362b06c37858b245c5c148521533ae28168383b0
calcado <pecalcado@gmail.com>
2024-04-05 15:33:27 -0300

    fibonnaci refactoring

--- exercicio2/src/App.java
+++ exercicio2/src/App.java
@@ -1,27 +1,15 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class App {
     public static void main(String[] args){
         
-        List<Integer> fibonacci = new ArrayList<>();
-        fibonacci.add(0);
-        fibonacci.add(1);
-        int tamanhoArray = 15;
-        int primeiroIncide = 0;
-        int segundoIndice =1;
+        int x = 10;
+        int n1 = 0;
+        int n2 = 1; 
 
-
-        for(int i = 0 ; i < tamanhoArray; i++){
-            int soma = fibonacci.get(primeiroIncide) + fibonacci.get(segundoIndice);
-
-            System.out.print(fibonacci.get(primeiroIncide)+ " "); 
-
-            fibonacci.add(soma);  
-            primeiroIncide = fibonacci.size()-2;
-
-            segundoIndice = fibonacci.size()-1;
-                              
+        for(int qtd = x ; qtd > 0; qtd--){
+            System.out.println(n1 + "");
+            int n3 = n1 + n2;
+            n1 = n2;
+            n2 = n3;
         }      
 
     }
